b5b339b6acc953c91b3da61f5ff8871df4e92056
Oleksandr Sukhomlynov <Oleksandr_Sukhomlynov@epam.com>
2022-08-07 12:56:00 +0300

    Fixed test cases

--- src/test/java/com/epam/crpsrv/cryptopriceparser/CryptoPriceValuesParserImplTest.java
+++ src/test/java/com/epam/crpsrv/cryptopriceparser/CryptoPriceValuesParserImplTest.java
@@ -1,11 +1,17 @@
 package com.epam.crpsrv.cryptopriceparser;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.epam.crpsrv.cryptoprice.CryptoPriceDto;
 import com.epam.crpsrv.cryptoprice.CryptoPriceParserConfig;
 import com.epam.crpsrv.cryptoprice.CryptoPriceParserFactoryImpl;
 import com.epam.crpsrv.cryptoprice.parser.CryptoPriceDtoBuilderUnknown;
 import com.epam.crpsrv.cryptoprice.parser.CryptoPriceValuesParser;
 import com.epam.crpsrv.cryptoprice.parser.CryptoPriceValuesParserImpl;
+import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,7 +19,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
-@ContextConfiguration(classes = {CryptoPriceParserFactoryImpl.class, CryptoPriceValuesParserImpl.class, CryptoPriceDtoBuilderUnknown.class})
+@ContextConfiguration(classes = {CryptoPriceParserFactoryImpl.class, CryptoPriceValuesParserImpl.class,
+        CryptoPriceDtoBuilderUnknown.class})
 @Import(value = {CryptoPriceParserConfig.class})
 class CryptoPriceValuesParserImplTest {
 
@@ -27,7 +34,21 @@ class CryptoPriceValuesParserImplTest {
 
     @Test
     void parse() {
-        var actualCryptoPrices = cryptoPriceValuesParser.parse(BTC_VALUES.getBytes(StandardCharsets.UTF_8));
-        System.out.println(actualCryptoPrices);
+        var expectedResult = List.of(
+                CryptoPriceDto.builder()
+                        .symbol("BTC")
+                        .price(new BigDecimal("46813.21"))
+                        .timestamp(LocalDateTime.of(2022, 1, 1, 6, 0))
+                        .build(),
+                CryptoPriceDto.builder()
+                        .symbol("BTC")
+                        .price(new BigDecimal("46979.61"))
+                        .timestamp(LocalDateTime.of(2022, 1, 1, 9, 0))
+                        .build()
+        );
+
+        var actualResult = cryptoPriceValuesParser.parse(BTC_VALUES.getBytes(StandardCharsets.UTF_8));
+
+        assertThat(actualResult).isEqualTo(expectedResult);
     }
 }
--- src/test/java/com/epam/crpsrv/service/StatisticServiceImplTest.java
+++ src/test/java/com/epam/crpsrv/service/StatisticServiceImplTest.java
@@ -5,7 +5,10 @@ import static org.mockito.Mockito.doReturn;
 
 import com.epam.crpsrv.dto.NormalizedRangeDto;
 import com.epam.crpsrv.dto.OldestNewestMinMaxDto;
+import com.epam.crpsrv.model.view.NormalizedRangeView;
+import com.epam.crpsrv.model.view.OldestNewestMinMaxView;
 import com.epam.crpsrv.repository.CryptoPriceRepository;
+import com.epam.crpsrv.util.ViewDtoMapperUtil;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
@@ -29,15 +32,23 @@ class StatisticServiceImplTest {
         var dateFrom = LocalDate.of(2022, 1, 1);
         var dateTo = LocalDate.of(2022, 1, 10);
 
+        var symbol = "XRP";
+        var minPrice = new BigDecimal("0.8298");
+        var maxPrice = new BigDecimal("0.8337");
+
         var expectedDto = OldestNewestMinMaxDto.builder()
-                .minPrice(new BigDecimal("0.8298"))
-                .maxPrice(new BigDecimal("0.8337"))
-                .symbol("XRP")
+                .symbol(symbol)
+                .minPrice(minPrice)
+                .maxPrice(maxPrice)
                 .build();
 
         var expectedResult = List.of(expectedDto);
 
-        doReturn(expectedResult).when(cryptoPriceRepository).calcOldestNewestMinMax(dateFrom, dateTo);
+        var expectedViews = List.of(
+                createOldestNewestMinMaxViewInstance(symbol, minPrice, maxPrice)
+        );
+
+        doReturn(expectedViews).when(cryptoPriceRepository).calcOldestNewestMinMax(dateFrom, dateTo);
 
         var actualResult = statisticService.calcOldestNewestMinMax(dateFrom, dateTo);
 
@@ -52,15 +63,23 @@ class StatisticServiceImplTest {
         var dateFrom = LocalDate.of(2022, 1, 1);
         var dateTo = LocalDate.of(2022, 2, 1);
 
+        var symbol = "BTC";
+        var minPrice = new BigDecimal("46813.21");
+        var maxPrice = new BigDecimal("47336.98");
+
         var expectedDto = OldestNewestMinMaxDto.builder()
-                .minPrice(new BigDecimal("46813.21"))
-                .maxPrice(new BigDecimal("47336.98"))
-                .symbol("BTC")
+                .symbol(symbol)
+                .minPrice(minPrice)
+                .maxPrice(maxPrice)
                 .build();
 
         var expectedResult = List.of(expectedDto);
 
-        doReturn(expectedResult).when(cryptoPriceRepository).calcOldestNewestMinMax(dateFrom, dateTo);
+        var expectedViews = List.of(
+                createOldestNewestMinMaxViewInstance(symbol, minPrice, maxPrice)
+        );
+
+        doReturn(expectedViews).when(cryptoPriceRepository).calcOldestNewestMinMax(dateFrom, dateTo);
 
         var actualResult = statisticService.calcOldestNewestMinMax(month, year);
 
@@ -76,34 +95,70 @@ class StatisticServiceImplTest {
         var dateTo = LocalDate.of(2022, 2, 1);
 
         var symbol = "BTC";
+        var minPrice = new BigDecimal("35813.21");
+        var maxPrice = new BigDecimal("336.98");
 
         var expectedResult = OldestNewestMinMaxDto.builder()
-                .minPrice(new BigDecimal("35813.21"))
-                .maxPrice(new BigDecimal("336.98"))
                 .symbol(symbol)
+                .minPrice(minPrice)
+                .maxPrice(maxPrice)
                 .build();
 
-        doReturn(expectedResult).when(cryptoPriceRepository).calcOldestNewestMinMax(dateFrom, dateTo, symbol);
+        var expectedView = createOldestNewestMinMaxViewInstance(symbol, minPrice, maxPrice);
+        doReturn(expectedView).when(cryptoPriceRepository).calcOldestNewestMinMax(dateFrom, dateTo, symbol);
 
         var actualResult = statisticService.calcOldestNewestMinMax(month, year, symbol);
 
         assertThat(actualResult).isEqualTo(expectedResult);
     }
 
+    private OldestNewestMinMaxView createOldestNewestMinMaxViewInstance(
+            String symbol, BigDecimal minPrice, BigDecimal maxPrice) {
+
+        return new OldestNewestMinMaxView() {
+
+            @Override
+            public String getSymbol() {
+                return symbol;
+            }
+
+            @Override
+            public BigDecimal getMinPrice() {
+                return minPrice;
+            }
+
+            @Override
+            public BigDecimal getMaxPrice() {
+                return maxPrice;
+            }
+
+            @Override
+            public BigDecimal getOldestPrice() {
+                return null;
+            }
+
+            @Override
+            public BigDecimal getNewestPrice() {
+                return null;
+            }
+        };
+    }
+
     @Test
     void calcNormalizedPrice() {
-        var expectedResult = List.of(
-                NormalizedRangeDto.builder()
-                        .symbol("XRP")
-                        .normalizedRange(new BigDecimal("0.21"))
-                        .build(),
-                NormalizedRangeDto.builder()
-                        .symbol("BTC")
-                        .normalizedRange(new BigDecimal("0.55"))
-                        .build()
-        );
+        var symbolBTC = "BTC";
+        var normRangeBTC = new BigDecimal("0.55");
 
-        doReturn(expectedResult).when(cryptoPriceRepository).calcNormalizedRange();
+        var symbolXRP = "XRP";
+        var normRangeXRP = new BigDecimal("0.21");
+
+        var viewBTC = createNormRangeViewInstance(symbolBTC, normRangeBTC);
+        var viewXRP = createNormRangeViewInstance(symbolXRP, normRangeXRP);
+
+        var expectedViews = List.of(viewXRP, viewBTC);
+        var expectedResult = ViewDtoMapperUtil.normalizedRangeViewToDtoList(expectedViews);
+
+        doReturn(expectedViews).when(cryptoPriceRepository).calcNormalizedRange();
 
         var actualResult = statisticService.calcNormalizedRange();
 
@@ -113,17 +168,33 @@ class StatisticServiceImplTest {
     @Test
     void calcNormalizedPriceByCrypto() {
         var symbol = "XRP";
-        var expectedResult = NormalizedRangeDto.builder()
-                .symbol(symbol)
-                .normalizedRange(new BigDecimal("0.21"))
-                .build();
+        var normRange = new BigDecimal("0.21");
+
+        var expectedResult = new NormalizedRangeDto(symbol, normRange);
+        var expectedView = createNormRangeViewInstance(symbol, normRange);
 
         var date = LocalDate.of(2022, 1, 1);
 
-        doReturn(expectedResult).when(cryptoPriceRepository).calcNormalizedRange(date, symbol);
+        doReturn(expectedView).when(cryptoPriceRepository).calcNormalizedRange(date, symbol);
 
         var actualResult = statisticService.calcNormalizedRange(date, symbol);
 
         assertThat(actualResult).isEqualTo(expectedResult);
+    }
+
+    private NormalizedRangeView createNormRangeViewInstance(String symbol, BigDecimal normRange) {
+
+        return new NormalizedRangeView() {
+
+            @Override
+            public String getSymbol() {
+                return symbol;
+            }
+
+            @Override
+            public BigDecimal getNormalizedRange() {
+                return normRange;
+            }
+        };
     }
 }
